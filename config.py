f8f8210473b58e9ec6bc6b13fd0d9848886c848c
Maureen <murimimaureen69@gmail.com>
2020-04-26 02:08:24 +0300

    Add debug

--- config.py
+++ config.py
@@ -3,3 +3,13 @@ import os
 class Config:
     '''General configuration parent class
     '''
+    
+class ProdConfig:
+        '''Production  configuration child class
+        '''
+        pass
+    
+class DevConfig:
+     '''Development  configuration child class
+     '''
+     DEBUG = True
