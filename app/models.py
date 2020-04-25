c2c5abff5f33ea566fd71a27423b13a956cc7565
Maureen <murimimaureen69@gmail.com>
2020-04-26 01:49:09 +0300

    create class source

---
+++ app/models.py
@@ -0,0 +1,6 @@
+class Sources:
+    """ Sources to define news source objects
+    """
+    def __init__(self, id, description, url, category, name, country, language ):   
+        """Create instances for newsSource
+        """
