f36ba161023e24c9d256cf5a9a6f6633d53a5ef8
Maureen <murimimaureen69@gmail.com>
2020-04-26 02:03:12 +0300

    Add create_app to init

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,3 +1,10 @@
 from flask import Blueprint
 main = Blueprint('main', __name__)
 from .import views, errors
+
+def create_app(config_name):
+    app = Flask(__name__)
+    
+     #Create tha app configurations
+    app.config.from_object(config_options[config_name])
+    
