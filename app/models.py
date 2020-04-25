24a1b21a5166811de4fd188159c50e5273de05a9
Maureen <murimimaureen69@gmail.com>
2020-04-26 01:50:01 +0300

    Instantaite source class

--- app/models.py
+++ app/models.py
@@ -4,3 +4,10 @@ class Sources:
     def __init__(self, id, description, url, category, name, country, language ):   
         """Create instances for newsSource
         """
+        self.id = id
+        self.description = description
+        self.url = url
+        self.category = category
+        self.name = name
+        self.country = country
+        self.language = language
