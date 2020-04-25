19aa4d1f37d01032e4911ec0b0c07b7ad94aa02e
Maureen <murimimaureen69@gmail.com>
2020-04-26 01:54:27 +0300

    setup test-source

--- tests/test_Source.py
+++ tests/test_Source.py
@@ -4,3 +4,9 @@ from models import Sources, Articles
 class TestSource(unittest.TestCase):
   """Defines test for the NewsSource class behaviour
     """
+    def setUp(self):
+          '''
+        Set up method that will run before every Test
+        '''
+        self.new_source = Sources('CNN','Cable News Newtork that is a leader in providing news worldwide', 'cnn.com', 'general', 'CNN News', 'U.S.A', 'en')
+        
