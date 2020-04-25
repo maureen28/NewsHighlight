1dbd0a9e0751a87fe941c2a9c96b03909ebb0ec9
Maureen <murimimaureen69@gmail.com>
2020-04-26 01:55:20 +0300

    instance and init test

--- tests/test_Source.py
+++ tests/test_Source.py
@@ -9,4 +9,11 @@ class TestSource(unittest.TestCase):
         Set up method that will run before every Test
         '''
         self.new_source = Sources('CNN','Cable News Newtork that is a leader in providing news worldwide', 'cnn.com', 'general', 'CNN News', 'U.S.A', 'en')
-        
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source, Sources))
+    
+    def test_init(self):
+          '''
+        test_init test case to test if the object is initialized properly
+        '''
