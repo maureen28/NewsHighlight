160651475082b6ec8f16f1939fb1090557a9d407
Maureen <murimimaureen69@gmail.com>
2020-04-26 16:56:43 +0300

    Create index function

--- app/main/views.py
+++ app/main/views.py
@@ -2,3 +2,17 @@ from flask import render_template, request, redirect, url_for
 from . import main
 from ..models import Articles, Sources
 from ..requests import get_sources,get_article,search_everything
+
+# Views
+@main.route('/')
+def index(): 
+    '''
+    View root page function that returns the index page and its data
+    '''
+    sources = get_sources('business')
+    entertainment_sources = get_sources('entertainment')
+	sports_sources = get_sources('sports')
+	technology_sources = get_sources('technology')
+	health_sources = get_sources('health')
+ 
+	title = "News Highlight"
