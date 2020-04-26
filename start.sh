7baa3d51e138ffca5a6aeb5d5ebf2f4f1f134607
Maureen <murimimaureen69@gmail.com>
2020-04-26 19:52:31 +0300

    fix config bug

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
-export MOVIE_API_KEY=<'78068708007e4e098058f202b1c680d0'>
-export SECRET_KEY=<'WOAH'>
-
+export MOVIE_API_KEY = '78068708007e4e098058f202b1c680d0'
+export SECRET_KEY = 'WOAH'
 python3.6 manage.py server
