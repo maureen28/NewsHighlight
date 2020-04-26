5cf64acbdac7769a7d88f1c0b8c68a9264deccca
Maureen <murimimaureen69@gmail.com>
2020-04-26 21:46:46 +0300

    Fix import error

--- app/requests.py
+++ app/requests.py
@@ -16,7 +16,7 @@ everything_search_url = None
 
        
 def configure_request(app):
-        global api_key,base_url,articles_url,everything_search_url
+        global api_key, base_url, articles_url, everything_search_url
         api_key = app.config['NEWS_API_KEY']
         base_url = app.config['NEWS_SOURCES_BASE_URL']
         articles_url = app.config['ARTICLES_BASE_URL']
