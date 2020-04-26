948d5524ab1ad93104f08d5ebc14ab7023d2a7ed
Maureen <murimimaureen69@gmail.com>
2020-04-26 19:55:30 +0300

    import urllib and add it

---
+++ app/requests.py
@@ -0,0 +1,14 @@
+import urllib.request, json
+from .models import Sources,Articles
+
+# Getting api key
+api_key = None
+
+# Getting the base url
+base_url = None
+
+# Getting the article
+articles_url = None
+
+# search
+everything_search_url = None
