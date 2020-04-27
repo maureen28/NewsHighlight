56582e880bc20566f0bd9c8bb75735cf0561c434
Maureen <murimimaureen69@gmail.com>
2020-04-28 01:24:47 +0300

    Unittesting

--- tests/test_Source.py
+++ tests/test_Source.py
@@ -1,7 +1,7 @@
 import unittest
-from models import Sources, Articles
+from models import News, Sources
 
-class TestSource(unittest.TestCase):
+class TestNews(unittest.TestCase):
   """Defines test for the NewsSource class behaviour
     """
 def setUp(self):
@@ -9,7 +9,7 @@ def setUp(self):
     Set up method that will run before every Test
     '''
     
-    self.new_source = Sources('CNN','Cable News Newtork that is a leader in providing news worldwide', 'cnn.com', 'general', 'CNN News', 'U.S.A','en')
+    self.new_source = News()
 
 
 def test_instance(self):
@@ -19,12 +19,12 @@ def test_init(self):
       '''
     test_init test case to test if the object is initialized properly
     '''
-    self.assertEqual(self.new_source.author, 'CNN')
-    self.assertEqual(self.new_source.description, 'Cable News Newtork that is a leader in providing news worldwide')
-    self.assertEqual(self.new_source.url, 'cnn.com')
-    self.assertEqual(self.new_source.urlToImage, 'general')
-    self.assertEqual(self.new_source.title, 'CNN News')
-    self.assertEqual(self.new_source.content, 'U.S.A')
+    self.assertEqual(self.new_source.author, 'Peter Polle')
+    self.assertEqual(self.new_source.title, 'The tech scene in Africa-Is it the next big thing?')
+    self.assertEqual(self.new_source.description, 'A look at various tech hubs in Africa and the impact they have on the worlds economy')
+    self.assertEqual(self.new_source.url, 'techie.com')
+    self.assertEqual(self.new_source.urlToImage, 'techie.com/7643t94.jpg')
+    self.assertEqual(self.new_source.content, 'One of the videos shows an incident from 2004, and the other two were recorded in January 2015, according to Sue Gough, a Defense Department spokeswoman. The videos became public after unauthorized leaks in 2007 and 2017, and the Navy previously ')
  
 
 # Article
@@ -37,7 +37,7 @@ def setUp(self):
           '''
         Set up method that will run before every Test
         '''
-    self.new_article = Articles('CNN','Georgia starts to reopen but nervous mayors warn that coronavirus crisis isn"t over', 'Maeve Reston', 'edition.cnn.com', 'Global coronavirus pandemic', '2020-04-25T14:15:36Z', 'coronavirus.jpg')
+    self.new_article = Sources('CNN','Georgia starts to reopen but nervous mayors warn that coronavirus crisis isn"t over', 'Maeve Reston', 'edition.cnn.com', 'Global coronavirus pandemic', '2020-04-25T14:15:36Z', 'coronavirus.jpg')
     
 def test_instance(self):
     self.assertTrue(isinstance(self.new_article, Articles))
@@ -47,9 +47,7 @@ def test_init(self):
     test_init test case to test if the object is initialized properly
     '''
     self.assertEqual(self.new_article.id, 'CNN')
-    self.assertEqual(self.new_article.title, 'Georgia starts to reopen but nervous mayors warn that coronavirus crisis isn"t over')
-    self.assertEqual(self.new_article.author, 'Maeve Reston')
-    self.assertEqual(self.new_article.url, 'edition.cnn.com')
-    self.assertEqual(self.new_article.description, 'Global coronavirus pandemic')
-    self.assertEqual(self.new_article.date, '2020-04-25T14:15:36Z')
-    self.assertEqual(self.new_article.image, 'coronavirus.jpg')
+    self.assertEqual(self.new_article.name, 'CNN News')
+    self.assertEqual(self.new_article.description, 'Cable News Newtork that is a leader in providing news worldwide')
+    self.assertEqual(self.new_article.url, 'cnn.com')
+    self.assertEqual(self.new_article.category, 'general')
